dfd675183673da784d947f055bdbfb34aa398ff5
rinehaxor <rinehaxor@users.noreply.github.com>
2023-03-11 17:40:29 +0700

    React Fundamental

---
+++ 14_React Fundamental/Praktikum/vite-project/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <button type="submit" id="submit_button" className="btn btn-primary w-100 mb-5">
+      Submit
+    </button>
+  );
+}
